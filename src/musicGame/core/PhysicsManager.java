f3d4a73c39a2e76d769052f6aafeafce3237325e
Leo Denault <leo.denault@gmail.com>
2015-09-09 19:39:20 -0400

    Removed unused coord tracking in physics manager

--- src/musicGame/core/PhysicsManager.java
+++ src/musicGame/core/PhysicsManager.java
@@ -1,13 +1,10 @@
 package musicGame.core;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 import musicGame.geometry.Rectangle;
-
-import org.newdawn.slick.geom.Vector2f;
 
 public class PhysicsManager {
 	
@@ -33,12 +30,10 @@ public class PhysicsManager {
 	
 	private List<StaticCollidable> statics;
 	private List<DynamicCollidable> dynamics;
-	private HashMap<DynamicCollidable, Vector2f> coords;
 	
 	public PhysicsManager() {
 		statics = new LinkedList<StaticCollidable>();
 		dynamics = new LinkedList<DynamicCollidable>();
-		coords = new HashMap<DynamicCollidable, Vector2f>();
 	}
 	
 	public void addStaticCollidable(StaticCollidable collidable) {
@@ -47,7 +42,6 @@ public class PhysicsManager {
 	
 	public void addDynamicCollidable(DynamicCollidable collidable) {
 		dynamics.add(collidable);
-		coords.put(collidable, new Vector2f(collidable.getFloatX(), collidable.getFloatY()));
 	}
 	
 	public void addStaticCollidables(Collection<? extends StaticCollidable> collidables) {
@@ -56,16 +50,11 @@ public class PhysicsManager {
 	
 	public void addDynamicCollidables(Collection<? extends DynamicCollidable> collidables) {
 		dynamics.addAll(collidables);
-		
-		for (DynamicCollidable collidable : collidables) {
-			coords.put(collidable, new Vector2f(collidable.getFloatX(), collidable.getFloatY()));
-		}
 	}
 	
 	public void update(int delta) {
 		applyGravity(delta);
 		checkCollisions();
-		updatePositions();
 	}
 	
 	private void applyGravity(int delta) {
@@ -128,12 +117,6 @@ public class PhysicsManager {
 					}
 				}
 			}
-		}
-	}
-	
-	private void updatePositions() {
-		for (DynamicCollidable child : dynamics) {
-			coords.put(child, new Vector2f(child.getFloatX(), child.getFloatY()));
 		}
 	}
 }
