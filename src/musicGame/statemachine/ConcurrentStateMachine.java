e3fee8fd3be59691c7db8a08d2b386bb925fcd68
Leo Denault <leo.denault@gmail.com>
2017-07-16 22:25:04 -0400

    Added ability to listen to state machine transitioning.

--- src/musicGame/statemachine/ConcurrentStateMachine.java
+++ src/musicGame/statemachine/ConcurrentStateMachine.java
@@ -1,15 +1,22 @@
 package musicGame.statemachine;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import musicGame.statemachine.State.Action;
 
 public class ConcurrentStateMachine {
+	public interface Listener {
+		void transitionEnded(Set<State> currentStates);
+	}
+	
 	private Set<State> concurrentStates;
+	private Set<Listener> listeners;
 
 	public ConcurrentStateMachine(Set<State> concurrentStartStates) {
 		concurrentStates = concurrentStartStates;
+		listeners = new HashSet<>();
 	}
 
 	public Set<State> getStates() {
@@ -20,6 +27,11 @@ public class ConcurrentStateMachine {
 		concurrentStates = concurrentStates.stream()
 				.map(state -> state.transition(action))
 				.collect(Collectors.toSet());
+		listeners.stream().forEach(listener -> listener.transitionEnded(concurrentStates));
 		return concurrentStates;
+	}
+	
+	public void addListener(Listener listener) {
+		listeners.add(listener);
 	}
 }
--- test/musicGame/statemachine/ConcurrentStateMachineTest.java
+++ test/musicGame/statemachine/ConcurrentStateMachineTest.java
@@ -1,12 +1,12 @@
 package musicGame.statemachine;
 
+import static musicGame.core.Util.setOf;
 import static musicGame.statemachine.State.Action.JUMP;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
-
-import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,17 +17,16 @@ public class ConcurrentStateMachineTest {
 	@Mock private State state1;
 	@Mock private State state2;
 	@Mock private State state3;
+	@Mock private ConcurrentStateMachine.Listener listener;
 
 	private ConcurrentStateMachine machine;
 
-	@SuppressWarnings("serial")
 	@Before
 	public void setUp() {
 		initMocks(this);
-		machine = new ConcurrentStateMachine(new HashSet<State>() {{
-			add(state1);
-			add(state3);
-		}});
+		machine = new ConcurrentStateMachine(setOf(state1, state3));
+		when(state1.transition(JUMP)).thenReturn(state2);
+		when(state3.transition(JUMP)).thenReturn(state3);
 	}
 
 	@Test
@@ -37,9 +36,14 @@ public class ConcurrentStateMachineTest {
 
 	@Test
 	public void transitionCorrectlyTransitionsForAllStates() {
-		when(state1.transition(JUMP)).thenReturn(state2);
-		when(state3.transition(JUMP)).thenReturn(state3);
 		assertThat(machine.transition(JUMP), containsInAnyOrder(state2, state3));
 		assertThat(machine.getStates(), containsInAnyOrder(state2, state3));
+	}
+	
+	@Test
+	public void listenersAreUpdatedUponTransitioning() {
+		machine.addListener(listener);
+		machine.transition(JUMP);
+		verify(listener).transitionEnded(setOf(state2, state3));
 	}
 }
